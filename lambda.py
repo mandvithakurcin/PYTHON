a5409d87e9014a5b07a05ce45fe1f017039e3beb
mandvithakurcin <91454548+mandvithakurcin@users.noreply.github.com>
2021-10-19 13:42:14 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -12,17 +12,22 @@ print(x)
 list_1 = [1,2,3,4,5]
 
 filter( lambda x: x%2==0 , list_1 )
-print( list( filter( lambda x: x%2==0 , list_1 ))
+print( list( filter( lambda x: x%2==0 , list_1 )))
 
-
+#list lambda
 name=["mandvi","veli","shruti","Jay"]
 last_name=["thakur","sethiya","tiwari","Dubey"]
 l=list(map(lambda x,y:x+" "+y,name,last_name))
 print(l)
-['mandvi thakur','veli sethiya','shruti tiwari','Jay Dubey']
+
 
 # print table 2  lambda funtions 
  table=[lambda i=x:2*i for x in range(1,11)]
 
 for j in table:
     print(j())
+  
+# list 
+list1 = [1, 3, 5, 7, 9]
+list2 = list(map(lambda x: x * 2 , list1))
+print(list2)
