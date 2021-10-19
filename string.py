58491a98ae60719b9472cf5852d55a74dedfbfab
mandvithakurcin <91454548+mandvithakurcin@users.noreply.github.com>
2021-10-19 14:57:31 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,124 +1,38 @@
-print("Hello")
-a=46
-b=33
-a=b
-print (a)
-print(a-b)
-print(a+b)
-print(a/b)
-print(a*b)
-b=10
-print(a-b)
-print(a)
-print(a**b)
-print(a<b)
-print(a>b)
-print(a!=b)
-a= True
-print(a)
-b=False
-print(b)
-print(a&b)
-print(a|b)
-print(a|a)
-print(b|b)
-print ("rint("Hello")
-a=46
-b=33
-a=b
-print (a)
-print(a-b)
-print(a+b)
-print(a/b)
-print(a*b)
-b=10
-print(a-b)
-print(a)
-print(a**b)
-print(a<b)
-print(a>b)
-print(a!=b)
-a= True
-print(a)
-b=False
-print(b)
-print(a&b)
-print(a|b)
-print(a|a)
-print(b|b)
-print ("rint("Hello")
-a=46
-b=33
-a=b
-print (a)
-print(a-b)
-print(a+b)
-print(a/b)
-print(a*b)
-b=10
-print(a-b)
-print(a)
-print(a**b)
-print(a<b)
-print(a>b)
-print(a!=b)
-a= True
-print(a)
-b=False
-print(b)
-print(a&b)
-print(a|b)
-print(a|a)
-print(b|b)
-print ("mandvi")
-str1 = "My name is mandvi"
-print(str1)
-str2 = '''My name is mandvi'''
-print(str2)
-first_string= "My Name Is mandvi"
-print(first_string[0])
-print(len(first_string))
-print(first_string[1:-5])
-print(first_string[1:5])
-print(first_string[1:-5])
-print(first_string[-1:5])
-print(first_string.upper())
-print(first_string)
-print(first_string.lower())
-print(first_string)
-print(first_string.replace('Is','Are'))
-print(first_string)")
-str1 = "My name is mandvi"
-print(str1)
-str2 = '''My name is mandvi'''
-print(str2)
-first_string= "My Name Is mandvi"
-print(first_string[0])
-print(len(first_string))
-print(first_string[1:-5])
-print(first_string[1:5])
-print(first_string[1:-5])
-print(first_string[-1:5])
-print(first_string.upper())
-print(first_string)
-print(first_string.lower())
-print(first_string)
-print(first_string.replace('Is','Are'))
-print(first_string)")
-str1 = "My name is mandvi"
-print(str1)
-str2 = '''My name is mandvi'''
-print(str2)
-first_string= "My Name Is mandvi"
-print(first_string[0])
-print(len(first_string))
-print(first_string[1:-5])
-print(first_string[1:5])
-print(first_string[1:-5])
-print(first_string[-1:5])
-print(first_string.upper())
-print(first_string)
-print(first_string.lower())
-print(first_string)
-print(first_string.replace('Is','Are'))
-print(first_string)
+#string
+
+string= 'Hello world'
+print(string)
+
+#String Operations
+
+string1 = 'hello'
+string2 ='world'
+print( string1 + string2)
+
+#tripple qoutes
+print('''hello , 
+            world''')
+# Split String, replace , lower, upper , length 
+
+a = "Hello World"
+print(a.split(" .. ,  .."))
+
+
+
+a = "hello world"
+print(a.replace("e", "E"))
+
+
+
+a = "Hello, World"
+print(a.lower())
+
+
+
+a = "Hello, World"
+print(a.upper())
+
+a = "Hello, World"
+print(len(a))
+
+
