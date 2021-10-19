54280e9e74fe04293a4ae3c2ff144fec000c7039
mandvithakurcin <91454548+mandvithakurcin@users.noreply.github.com>
2021-10-19 14:27:54 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,61 +1,46 @@
-first_tuple=()
-print(first_tuple)
-secondtuple=(1,2,3,4,5)
-print(secondtuple)
-secondtuple=(1,2,3),"mandvi",(5,6,7)
-print(secondtuple)
-secondtuple=(1,2,3),"mandvi",(5,6,7),[6,8,9]
-print(secondtuple[0])
-print(secondtuple[3])
-print(secondtuple)
-secondtuple=33,'CSIT',2021
-print(secondtuple)
-a,b,c=secondtuple
+#empty tuple 
+tuple=()
+print(tuple)
+
+#tuple
+tuple=(1,2,3,4,5)
+print(tuple)
+
+#mixed data type 
+tuple=(1,2,3),"mandvi",(5,6,7)
+print(tuple)
+
+#indexing in tuple
+tuple=(1,2,3),"mandvi",(5,6,7),[6,8,9]
+print(tuple[0])
+print(tuple[3])
+
+#tuple unpacking 
+tuple=33,'CSIT',2021
+print(tuple)
+a,b,c=tuple
 print(a)
 print(b)
 print(c)
-a=33
-b='CSIT'
-c=2021
-print(a)
-print(secondtuple)
-print(type(secondtuple))
-secondtuple=('mandvi')
-print(type(secondtuple))
-secondtuple=(1,2,3),"Sanskar",(5,6,7),[6,8,9]
-print(secondtuple)
-print(secondtuple[0])
-print(secondtuplee[2])
-print(secondtuple[0][2])
-print(secondtuple[1][3])
-print(secondtuple[-1][-2])
-print(secondtuple[:])
-print(secondtuple[-2:-2])
-print(secondtuple[0:1])
-print(secondtuple[:-1])
-secondtuple=(1,2,8)+(2,7,4)
-print(secondtuple)
-print(('mandvi',)*5)
-print((95)*2)
-print(secondtuple)
-del(secondtuple)
-secondtuple=('S','a','n','s','k','a','r')
-print(secondtuple)
-print(secondtuple.count('a'))
-print(secondtuple.count('y'))
-print(secondtuple.index('a'))
-print('a' in secondtuple)
-print('z' in secondtuple)
-for n in ('mandvi','avdhi'):
-    print('hello',n)
-a=(6,8,9)
-for n in a:
-    print(2*n)
-x=[5,9,4]
-y.sort()
-print(x)
-x=sorted(y)
-y=(6,3,7)
-x=sorted(y)
-print(x)
-print(y)
+
+#slicing
+tuple=(1,2,3,4,5)
+print(tuple[-2:-2])
+# elements beginning to end
+print(tuple[:])
+
+#max, min , length
+tuple = (1,2,3,4,5)
+maximum = max(tuple)
+minimum = min(tuple)
+
+print("Max: ", maximum)
+print("Min: ",minimum)
+print("Len: ", len(tuple))
+
+#deleting a tuple
+tuple = (22,33,44,55,66,77)
+
+print(tuple)
+del tuple
+
