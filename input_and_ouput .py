c081b349a7d3fed8220c53e68f2be2d13d882cd7
mandvithakurcin <91454548+mandvithakurcin@users.noreply.github.com>
2021-10-19 14:40:09 +0530

    Create input_and_ouput .py

---
+++ input_and_ouput .py
@@ -0,0 +1,16 @@
+#Input command
+Name = input("Enter name: ")
+
+#Output command
+print("Name- " + Name)
+
+#split commands
+
+print("Split name with space ",Name.split())
+
+#Format command
+print("i love {} ".format("food"))
+
+#use of multiple format commands
+string = "{} love {}"
+print(string.format("i","food"))
