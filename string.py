22b11e29f442863e2cf6df462d22e4a73f56e7d6
mandvithakurcin <91454548+mandvithakurcin@users.noreply.github.com>
2021-12-07 01:04:53 +0530

    Update string.py

--- string.py
+++ string.py
@@ -35,4 +35,21 @@ print(a.upper())
 a = "Hello, World"
 print(len(a))
 
+print ("mandvi")
+str1 = "this is my string"
+print(str1)
+str2 = '''This
+ is my string'''
+print(str2)
+
+my_string= "My name is mandvi"
+print(my_string[0])
+
+print(len(my_string))
+print(my_string[1:-5])
+print(my_string[1:5])
+print(my_string[1:-5])
+print(my_string[-1:5])
+
+print(my_string.replace('is','are'))
 
