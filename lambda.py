9d2318c0c2c59e403faf93bd94fd96a7a384f9e2
mandvithakurcin <91454548+mandvithakurcin@users.noreply.github.com>
2021-12-07 01:18:56 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -31,3 +31,28 @@ for j in table:
 list1 = [1, 3, 5, 7, 9]
 list2 = list(map(lambda x: x * 2 , list1))
 print(list2)
+
+#adding list
+list1=[2,3,4,5,6]
+list2=[4,5,6,7,8]
+print(list(map (lambda x,y:x+y,list1,list2)))
+
+max=reduce (lambda x,y: x if x>y else y,list1)
+print(max)
+print("mandvi".upper())
+
+def func1(x):
+  return x+5
+
+print(func1(5))
+
+print(list(map(lambda x:pow(x,3), list1)))
+
+str1= ['hii', 'hello']
+print(list(map(lambda x: x.upper(), str1)))
+
+age=[5,6,7,8,9,10,11]
+print(list(filter(lambda x: x<10, age)))
+
+mylist = map(lambda x : x*2, [x for x in range(1,11)])
+print(list(mylist))
